c5c5973d300d362e9b08d017bdc426d0e7e43a81
Alejandra Gonzalez <alejandragm929@gmail.com>
2023-06-03 18:09:00 -0500

    javascript transacciones

--- src/js/script.js
+++ src/js/script.js
@@ -55,25 +55,25 @@ const agregarCaracter = (cadena, caracter, pasos) => {
 
 const show = (id) => {
   if (id == 'productos') {
-    document.getElementById(`${id}`).style.display='block'
-    document.getElementById('transacciones').style.display='none'
-    document.getElementById('movimientos').style.display='none'
+    document.getElementById(`${id}`).style.display = 'block'
+    document.getElementById('transacciones').style.display = 'none'
+    document.getElementById('movimientos').style.display = 'none'
   }
   else if (id == 'transacciones') {
-    document.getElementById(`${id}`).style.display='block'
-    document.getElementById('productos').style.display='none'
-    document.getElementById('movimientos').style.display='none'
-    
+    document.getElementById(`${id}`).style.display = 'block'
+    document.getElementById('productos').style.display = 'none'
+    document.getElementById('movimientos').style.display = 'none'
+
   } else {
-    document.getElementById(`${id}`).style.display='block'
-    document.getElementById('productos').style.display='none'
-    document.getElementById('transacciones').style.display='none'
+    document.getElementById(`${id}`).style.display = 'block'
+    document.getElementById('productos').style.display = 'none'
+    document.getElementById('transacciones').style.display = 'none'
   }
 }
 
 //Script tabla movimientos
 function showTransactionDetail(id) {
-  let transactionDetail= document.getElementById("detail-" + id);
+  let transactionDetail = document.getElementById("detail-" + id);
   if (transactionDetail.classList.contains("hidden")) {
     transactionDetail.classList.remove("hidden");
   } else {
@@ -82,21 +82,82 @@ function showTransactionDetail(id) {
 }
 
 
-// Obtener el formulario
-const loginText = document.querySelector(".title-text .login");
-const loginForm = document.querySelector("form.login");
-const loginBtn = document.querySelector("label.login");
-const signupBtn = document.querySelector("label.signup");
-const signupLink = document.querySelector("form .signup-link a");
-signupBtn.onclick = (()=>{
-  loginForm.style.marginLeft = "-50%";
-  loginText.style.marginLeft = "-50%";
-});
-loginBtn.onclick = (()=>{
-  loginForm.style.marginLeft = "0%";
-  loginText.style.marginLeft = "0%";
-});
-signupLink.onclick = (()=>{
-  signupBtn.click();
-  return false;
-});
+// // Obtener el formulario
+// const loginText = document.querySelector(".title-text .login");
+// const loginForm = document.querySelector("form.login");
+// const loginBtn = document.querySelector("label.login");
+// const signupBtn = document.querySelector("label.signup");
+// const signupLink = document.querySelector("form .signup-link a");
+// signupBtn.onclick = (() => {
+//   loginForm.style.marginLeft = "-50%";
+//   loginText.style.marginLeft = "-50%";
+// });
+// loginBtn.onclick = (() => {
+//   loginForm.style.marginLeft = "0%";
+//   loginText.style.marginLeft = "0%";
+// });
+// signupLink.onclick = (() => {
+//   signupBtn.click();
+//   return false;
+// });
+
+
+//// Script Transacciones
+  let balanceProducts = document.getElementById("balance");
+  let creditBalance = document.getElementById("credit");
+  let inputDeposit = document.getElementById("btnDeposit");
+  let inputWithdraw = document.getElementById("btnWithdraw");
+  let inputAdvance = document.getElementById("btnAdvance");
+
+  // funciones de los botones de avance, deposito y retiro
+
+  inputDeposit.onclick = () => {
+    let moneyInput = Number(document.getElementById("money").value);
+
+    let newBalance = usuario.saldo + moneyInput;
+    // balanceProducts.innerText = "$" + newBalance.toFixed(2);
+    usuario.saldo = newBalance;
+
+    console.log(moneyInput);
+    console.log(newBalance)
+    let movementH2 = document.getElementById("movement");
+    movementH2.innerText = "Realizaste un deposito de $" + moneyInput.toFixed(2);
+    onInit()
+    show('productos');
+
+  }
+
+  inputWithdraw.onclick = () => {
+    let moneyInput = Number(document.getElementById("money").value);
+    if (moneyInput > usuario.saldo) {
+      alert("Saldo insuficiente, no Puedes realizar esta transaccion");
+      return;
+
+    }
+    //let currentBalance = Number(balanceProducts.innerText.replace(/[\$.,]/g, ""));
+    let newBalance = usuario.saldo - moneyInput;
+    // balanceProducts.innerText = "$" + newBalance.toFixed(2);
+    usuario.saldo = newBalance
+    let movementH2 = document.getElementById("movement");
+    movementH2.innerText = "Realizaste un retiro de $" + moneyInput.toFixed(2);
+    onInit();
+    show('productos');
+
+  }
+
+
+
+  inputAdvance.onclick = () => {
+    let moneyInput = Number(document.getElementById("money").value);
+    if (moneyInput > 2000000) {
+      alert("Has alcanzado el monto maximo por transacción");
+      return
+    };
+
+    let newAdvance = currentAdvance + moneyInput
+    creditBalance.innerText = "$" + "-" + newAdvance.toFixed(2);
+    currentAdvance = newAdvance
+    let movementH2 = document.getElementById("movement");
+    movementH2.innerText = "Avanzaste $" + moneyInput.toFixed(2);
+    console.log(newAdvance)
+  }
